d2a01939ad396d8ca4aab249919a5dc310adcbc3
Jithin M <jithinmbalu14@gmail.com>
2022-05-22 18:12:12 +0530

    feat: Create empty views for bike list screen

---
+++ BikeStationFeature/Sources/BikeStationFeature/BikeStationList.swift
@@ -0,0 +1,22 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Jithin M on 22/05/22.
+//
+
+import SwiftUI
+
+@available(iOS 13.0.0, *)
+struct BikeStationList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+@available(iOS 13.0.0, *)
+struct BikeStationList_Previews: PreviewProvider {
+    static var previews: some View {
+        BikeStationList()
+    }
+}
