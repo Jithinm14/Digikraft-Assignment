12d8f9ec29040b1f33ee75e29d74db059f3b5b72
Jithin M <jithinmbalu14@gmail.com>
2022-05-22 17:21:15 +0530

    feat: Add dependency injection container

--- DependencyInjectionService/Sources/DependencyInjectionService/DependencyInjectionService.swift
+++ DependencyInjectionService/Sources/DependencyInjectionService/DependencyInjectionService.swift
@@ -1,6 +1,37 @@
-public struct DependencyInjectionService {
-    public private(set) var text = "Hello, World!"
+//
+//  File.swift
+//  
+//
+//  Created by Jithin M on 22/05/22.
+//
 
-    public init() {
+import Foundation
+import NetworkService
+
+public protocol DIContainerProtocol {
+    func registerDependency<Component>(type: Component.Type, component: Any)
+    func resolveDependency<Component>(type: Component.Type) -> Component?
+    func registerAppDependencies()
+}
+
+public final class DIContainer: DIContainerProtocol {
+    
+    static let shared = DIContainer()
+    
+    private init() {}
+    
+    var components: [String: Any] = [:]
+    
+    public func resolveDependency<Component>(type: Component.Type) -> Component? {
+        return components["\(type)"] as? Component
     }
+    
+    public func registerDependency<Component>(type: Component.Type, component: Any) {
+        components["\(type)"] = component
+    }
+    
+    public func registerAppDependencies() {
+        registerDependency(type: NetworkService.self, component: NetworkService())
+    }
+    
 }
