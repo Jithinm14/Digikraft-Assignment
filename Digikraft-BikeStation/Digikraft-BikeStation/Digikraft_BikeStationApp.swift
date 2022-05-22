3d4fbc34e4d5de33ad0ef8bceca4e9347253332c
Jithin M <jithinmbalu14@gmail.com>
2022-05-22 14:50:47 +0530

    feat: Create xcode project

---
+++ Digikraft-BikeStation/Digikraft-BikeStation/Digikraft_BikeStationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Digikraft_BikeStationApp.swift
+//  Digikraft-BikeStation
+//
+//  Created by Jithin M on 22/05/22.
+//
+
+import SwiftUI
+
+@main
+struct Digikraft_BikeStationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
